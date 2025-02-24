be8baf65a315a74863450c31ec53096b85a87ce2
IsaiahMar <87765689+IsaiahMar@users.noreply.github.com>
2025-02-24 10:23:32 -0500

    added some stuff

--- volunteer-village/routes/web.php
+++ volunteer-village/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\TeacherController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -22,13 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
-
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/teacher/home', [TeacherController::class, 'index'])->name('teacher.home');
+require __DIR__.'/auth.php';
