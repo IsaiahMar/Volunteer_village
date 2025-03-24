0a246abbdcdb496b92cd923938d50420fcd3d5b2
Shef77 <165204268+Shef77@users.noreply.github.com>
2025-03-24 11:11:49 -0400

    fixed css
    changed css to match teacher home in style instead of individual

--- volunteer-village/resources/views/student/StudentHome.blade.php
+++ volunteer-village/resources/views/student/StudentHome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Home - Volunteer Village</title>
-    <link href="{{ asset('css/StudentHome.css') }}" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
@@ -21,14 +21,14 @@
             <a href="{{ route('student.opportunity.board') }}">Opportunity Board</a>
         </div>
         <div class="content">
-            <h1>Impact Stream</h1>
+            <!-- <h1>Impact Stream</h1>
             <div class="impact-stream">
                 @foreach($verifiedHours as $hour)
                     <div class="post-box">
                         <h3>{{ $hour->caption }}</h3>
                         <img src="{{ asset('storage/' . $hour->image_path) }}" alt="Service Image">
                     </div>
-                @endforeach
+                @endforeach -->
             </div>
         
         </div>
